c2cd9f92bd2cb2f7289be10ad2c89924a3d3ee51
Doncho Minkov <DonchoMinkov@gmail.com>
2015-12-11 17:10:34 +0200

    Added demo for simple data binding

---
+++ 06. Data-binding/demos/SimpleDataBinding/ViewModels/CatViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleDataBinding.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+
+    public class CatViewModel
+    {
+        public string Name { get; set; }
+
+        public double Age { get; set; }
+
+        public string ImgUrl { get; set; }
+    }
+}
