9a9cf0e9111d632979ab3d269d47884cc0a48489
Doncho Minkov <DonchoMinkov@gmail.com>
2015-12-11 19:07:22 +0200

    Added demo for List data-binding

---
+++ 07. Data-binding-lists/demos/SimpleDataBindingLists/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleDataBindingLists.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var item in collection)
+            {
+                action(item);
+            }
+        }
+    }
+}
---
+++ 07. Data-binding-lists/demos/SimpleDataBindingLists/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿namespace SimpleDataBindingLists.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using Extensions;
+
+    public class MainPageViewModel
+    {
+        private ObservableCollection<string> names;
+
+        public IEnumerable<string> Names
+        {
+            get
+            {
+                if (this.names == null)
+                {
+                    this.names = new ObservableCollection<string>();
+                }
+
+                return this.names;
+            }
+            set
+            {
+                if (this.names == null)
+                {
+                    this.names = new ObservableCollection<string>();
+                }
+                this.names.Clear();
+
+                value.ForEach(this.names.Add);
+            }
+        }
+
+        public void AddName(string name)
+        {
+            this.names.Add(name);
+        }
+
+        public void DeleteName(string name)
+        {
+            this.names.Remove(name);
+        }
+    }
+}
